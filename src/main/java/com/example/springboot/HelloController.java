1589631ffa57d549efdcf2971d90c0e2a0f52c65
wuwei16 <wuwei16@xiaomi.com>
2024-12-20 20:30:08 +0800

    init

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -3,11 +3,14 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
+		System.out.println(new Date());
 		return "Greetings from Spring Boot!";
 	}
 
